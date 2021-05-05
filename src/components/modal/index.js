f40aa510cef99a36521623b546d826f00aba89de
Christian Kleinboelting <kleinboelting@signal7.de>
2021-05-06 00:47:25 +0200

    Extract toggleModal and remove unused styling

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -5,17 +5,14 @@ import style from './style.css';
 const Modal = ({ buttontxt = "Click Me.", header, content, footer }) => {
 
     const [hideModal, setHideModal] = useState(true);
+    const toggleModal = () => setHideModal((showModal) => !showModal);
     return (
         <span>
-            <a href="#"
-                onClick={() => setHideModal((showModal) => !showModal)}
-                class={style.openmodal}>{buttontxt}</a>
-            < div className={`${style.modaloverlay} ${hideModal ? style.hidden : ""}`}>
+            <a href="#" onClick={toggleModal}>{buttontxt}</a>
+            <div className={`${style.modaloverlay} ${hideModal ? style.hidden : ""}`}>
                 <div className={style.modaldialog}>
                     <div className={style.modalheader}>
-                        <a href="#"
-                            onClick={() => setHideModal((showModal) => !showModal)}
-                            class={style.closemodal}>X</a>
+                        <a href="#" onClick={toggleModal} class={style.closemodal}>X</a>
                         <h1>{header}</h1>
                     </div>
                     <div className={style.modalbody}>
