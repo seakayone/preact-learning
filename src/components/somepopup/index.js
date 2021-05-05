21f1185f295aa6a0cd4eb595911666b19a1d2d61
Christian Kleinboelting <kleinboelting@signal7.de>
2021-05-06 00:19:20 +0200

    add modaloverlay

--- src/components/somepopup/index.js
+++ src/components/somepopup/index.js
@@ -10,17 +10,19 @@ const Somepopup = () => {
             <a href="#"
                 onClick={() => setHideModal((showModal) => !showModal)}
                 class={style.openmodal}>Click me</a>
-            <div className={`${style.modaldialog} ${hideModal ? style.hidden : ""}`}>
-                <div className={style.modalheader}>
-                    <a href="#"
-                        onClick={() => setHideModal((showModal) => !showModal)}
-                        class={style.closemodal}>X</a>
-                    <h1>Hello Modal</h1>
+            <div className={`${style.modaloverlay} ${hideModal ? style.hidden : ""}`}>
+                <div className={style.modaldialog}>
+                    <div className={style.modalheader}>
+                        <a href="#"
+                            onClick={() => setHideModal((showModal) => !showModal)}
+                            class={style.closemodal}>X</a>
+                        <h1>Hello Modal</h1>
+                    </div>
+                    <div className={style.modalbody}>
+                        Clicked {hideModal ? "true" : "falsch"} times.
                 </div>
-                <div className={style.modalbody}>
-                    Clicked {hideModal ? "true" : "falsch"} times.
+                    <div className={style.modalfooter}></div>
                 </div>
-                <div className={style.modalfooter}></div>
             </div>
         </div>
     );
