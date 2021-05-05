8f13f9a610d7a69d155a978d76f4db34e0781a4e
Christian Kleinboelting <kleinboelting@signal7.de>
2021-05-06 01:46:33 +0200

    Close Modal if click outside is detected

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,18 +1,34 @@
 import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { useState, useRef, useEffect } from 'preact/hooks';
 import style from './style.css';
+
+function useOnClickOutsideRef(callback, isHidden, initialValue = null) {
+    const elementRef = useRef(initialValue)
+    useEffect(() => {
+        function handler(event) {
+            if (!isHidden && !elementRef.current?.contains(event.target)) {
+                callback()
+            }
+        }
+        window.addEventListener('click', handler)
+        return () => window.removeEventListener('click', handler)
+    }, [callback])
+    return elementRef
+}
 
 const Modal = ({ buttontxt = "Click Me.", header, content, footer }) => {
 
     const [hideModal, setHideModal] = useState(true);
-    const toggleModal = () => setHideModal((showModal) => !showModal);
+    const closeModal = () => setHideModal(true);
+    const openModal = () => setHideModal(false);
+    const modalRef = useOnClickOutsideRef(closeModal, hideModal)
     return (
-        <span>
-            <a href="#" onClick={toggleModal}>{buttontxt}</a>
+        <>
+            <a href="#" onClick={openModal}>{buttontxt}</a>
             <div className={`${style.modaloverlay} ${hideModal ? style.hidden : ""}`} >
-                <div className={style.modaldialog}>
+                <div id="modal" ref={modalRef} className={style.modaldialog}>
                     <div className={style.modalheader}>
-                        <a href="#" onClick={toggleModal} class={style.closemodal}>X</a>
+                        <a href="#" onClick={closeModal} class={style.closemodal}>X</a>
                         <h1>{header}</h1>
                     </div>
                     <div className={style.modalbody}>
@@ -23,7 +39,7 @@ const Modal = ({ buttontxt = "Click Me.", header, content, footer }) => {
                     </div>
                 </div>
             </div>
-        </span>
+        </>
     );
 };
 
